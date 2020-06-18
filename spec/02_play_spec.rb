88f63c5325236a5fbce221928ca40d2908dac67c
Leslie G <lesliegonzalez@me.com>
2020-06-18 22:03:43 +0000

    Automatically backed up by Learn

--- spec/02_play_spec.rb
+++ spec/02_play_spec.rb
@@ -118,7 +118,8 @@ describe './lib/tic_tac_toe.rb' do
     it 'plays through an entire game' do
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       allow($stdout).to receive(:puts)
-
+      
+    # expect(self).to receive(:turn).at_most(1000).times
       expect(self).to receive(:gets).and_return("1")
       expect(self).to receive(:gets).and_return("2")
       expect(self).to receive(:gets).and_return("3")
